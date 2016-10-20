5681427754360bff2d62efd2e0241b4fda223f95
Stefan Wiehler <sephalon@farce.de>
2016-10-20 22:31:54 +0200

    Improved style and upgraded to new version

--- pkgs/development/misc/esp-open-sdk/default.nix
+++ pkgs/development/misc/esp-open-sdk/default.nix
@@ -1,33 +1,32 @@
-{ fetchgit, autoconf, stdenv, git,
-  help2man, lib, makeWrapper, fetchurl, writeScript
-, gcc, m4, perl, which, gperf, bison, flex, texinfo, wget, libtool, automake
-, ncurses, file, unzip, python, expat}:
+{ stdenv, lib, makeWrapper, fetchgit, fetchurl, writeScript, git
+, help2man, gcc, m4, perl, which, gperf, bison, flex, texinfo, wget, libtool
+, ncurses, file, unzip, python, expat, autoconf, automake
+}:
 
 stdenv.mkDerivation rec {
-  rev = "89ecaadb99e6006644da7635cd447007090d2fca";
+  rev = "24f10eb164947fdd38b6225f72e752d5eb785391";
   name = "esp-open-sdk-${rev}";
-
-cg-ng-esp = import ./crosstool-ng-esp {
-  inherit fetchgit autoconf help2man stdenv lib makeWrapper fetchurl writeScript
- gcc m4 perl which gperf bison flex texinfo wget libtool automake
- ncurses file unzip python expat;
-
-};
-
 
   src = fetchgit {
     url = "https://github.com/pfalcon/esp-open-sdk";
     rev = rev;
-    sha256 = "04yzmr7zp1f3i3ixxrcr63ywq515b05yjzfba8s0rrabla6vdzkp";
+    sha256 = "1ssklx9r2rwd364hl5gwb7jhk7apw3gk39h8qbs0hgjhci7nrqzv";
     deepClone = true;
   };
+
+  cg-ng-esp = import ./crosstool-ng-esp {
+    inherit stdenv lib makeWrapper fetchgit fetchurl writeScript
+    gcc m4 perl which gperf bison flex texinfo wget libtool automake
+    ncurses file unzip python expat autoconf help2man;
+    sdkRev = rev;
+  };
+
+  buildInputs = [ autoconf cg-ng-esp ];
 
   # GCC won't build otherwise
   hardeningDisable = [ "format" ];
 
   patches = [ ./crosstool-NG.patch ];
-
-  buildInputs = [ autoconf cg-ng-esp ];
 
   installPhase = ''
     mkdir -p $out
