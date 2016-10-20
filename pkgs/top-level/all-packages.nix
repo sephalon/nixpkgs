9695bae9bda557a745c71450b0f3b1d0f8d9475e
Stefan Wiehler <sephalon@farce.de>
2016-10-20 23:15:45 +0200

    Added esptool2

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1520,6 +1520,8 @@ in
 
   eventstat = callPackage ../os-specific/linux/eventstat { };
 
+  esptool2 = callPackage ../development/tools/misc/esptool2 { };
+
   evtest = callPackage ../applications/misc/evtest { };
 
   exa = callPackage ../tools/misc/exa { };
